bf5a63ab9b6139cefdca2d46026d3b1cc2b58f72
mathfreak64 <ivanop746@gmail.com>
2026-05-27 13:44:34 +0200

    Add new example Endpoints

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -34,4 +34,45 @@ app.MapGet("/check",
     }
 });
 
+// Insert Movie
+// Wenn Objekt eingefügt werden konnte,
+// wird es mit Statuscode 201 (Created) zurückgegeben.
+// Bei Fehler wird Statuscode 409 (Conflict) zurückgegeben.
+app.MapPost("/api/movies", (Movie movie) =>
+{
+    throw new NotImplementedException();
+});
+
+// Get all Movies
+// Gibt alle vorhandenen Movie-Objekte mit Statuscode 200 Ok zurück.
+app.MapGet("/api/movies", () =>
+{
+    throw new NotImplementedException();
+});
+
+// Get Movie by id
+// Gibt das gewünschte Movie-Objekt mit Statuscode 200 OK zurück.
+// Bei ungültiger id wird Statuscode 404 not found zurückgegeben.
+app.MapGet("/api/movies/{id}", (string id) =>
+{
+    throw new NotImplementedException();
+});
+
+// Update Movie
+// Gibt das aktualisierte Movie-Objekt zurück.
+// Der erfolgreiche Update wird mit Statuscode 200 OK quittiert.
+// Bei ungültiger id wird Statuscode 404 not found zurückgegeben
+app.MapPut("/api/movies/{id}", (string id, Movie movie) =>
+{
+    throw new NotImplementedException();
+});
+
+// Delete Movie
+// Gibt bei erfolgreicher Löschung Statuscode 204 NoContent zurück.
+// Bei ungültiger id wird Statuscode 404 not found zurückgegeben.
+app.MapDelete("/api/movies/{id}", (string id) =>
+{
+    throw new NotImplementedException();
+});
+
 app.Run();
